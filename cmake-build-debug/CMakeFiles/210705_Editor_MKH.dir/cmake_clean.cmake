f1523dd3acd909435933caf7593dd1f78a4fdbe1
MaKanghyeon <kanghun9611@gmail.com>
2021-07-20 15:47:41 +0900

    solution

---
+++ cmake-build-debug/CMakeFiles/210705_Editor_MKH.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "210705_Editor_MKH.exe"
+  "210705_Editor_MKH.exe.manifest"
+  "210705_Editor_MKH.lib"
+  "210705_Editor_MKH.pdb"
+  "CMakeFiles/210705_Editor_MKH.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/210705_Editor_MKH.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,4 +1,56 @@
 //
 // Created by 82107 on 2021-07-20.
 //
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+int main(void) {
+    stack<int> s;
+    stack<char>t;
+    string string="";
+    int num;
+    cin >> string;
+    cin >> num;
+    for (int i = 0; i < string.size(); i++) {
+        s.push(string[i]);
+    }
+    while (num--) {
+        char ctr;
+        cin >> ctr;
+        if (ctr == 'P') {
+            char c;
+            cin >> c;
+            s.push(c);
+        }
+        else if (ctr == 'L')
+        { if (s.empty()) continue;
+            else {
+                t.push(s.top());
+                s.pop();
+            }
+        }
+
+        else if (ctr == 'B') {
+            if (s.empty()) continue;
+            else s.pop();
+        }
+        else if (ctr == 'D') {
+            if (t.empty()) continue;
+            else {
+                s.push(t.top());
+                t.pop();
+            }
+        }
+    }
+    while (!s.empty()) {
+        t.push(s.top());
+        s.pop();
+    }
+    while (!t.empty()) {
+        cout << t.top();
+        t.pop();
+    }
+    return 0;
+}
 
